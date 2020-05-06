9cd9ffe979f5f3d569125b43250b54fccf4c3394
ArgjentaGashi <58112102+ArgjentaGashi@users.noreply.github.com>
2020-05-06 23:07:40 +0200

    Update ImportKey.java

--- ds/src/ds/ImportKey.java
+++ ds/src/ds/ImportKey.java
@@ -5,13 +5,13 @@ import java.nio.file.Paths;
 public class ImportKey {
     static public void ImportKeyPublic(String teksti1, String teksti2) throws IOException {
 
-        if (Files.exists(Paths.get("C:\\Users\\Asus\\IdeaProjects\\ProjektiSiguri\\Key\\" + teksti2+".pub.xml"))) {
+        if (Files.exists(Paths.get("keys\\" + teksti2+".pub.xml"))) {
             InputStream is = null;
             OutputStream os = null;
 
                 try {
-                    is = new FileInputStream("C:\\Users\\Asus\\IdeaProjects\\ProjektiSiguri\\Key\\" + teksti2 + ".pub.xml");
-                    os = new FileOutputStream("C:\\Users\\Asus\\IdeaProjects\\ProjektiSiguri\\Key\\" + teksti1 + ".pub.xml");
+                    is = new FileInputStream("keys\\" + teksti2 + ".pub.xml");
+                    os = new FileOutputStream("keys\\" + teksti1 + ".pub.xml");
                     byte[] buffer = new byte[1024];
                     int length;
                     while ((length = is.read(buffer)) > 0) {
@@ -26,13 +26,13 @@ public class ImportKey {
                 System.out.println("Ky celes nuk ekziston");
         }
         static public void ImportKeyPrivate(String teksti1, String teksti2) throws IOException {
-            if (Files.exists(Paths.get("C:\\Users\\Asus\\IdeaProjects\\ProjektiSiguri\\Key\\" + teksti2+".xml"))) {
+            if (Files.exists(Paths.get("keys\\" + teksti2+".xml"))) {
             InputStream is = null;
             OutputStream os = null;
 
                 try {
-                    is = new FileInputStream("C:\\Users\\Asus\\IdeaProjects\\ProjektiSiguri\\Key\\" + teksti2 + ".xml");
-                    os = new FileOutputStream("C:\\Users\\Asus\\IdeaProjects\\ProjektiSiguri\\Key\\" + teksti1 + ".xml");
+                    is = new FileInputStream("keys\\" + teksti2 + ".xml");
+                    os = new FileOutputStream("keys\\" + teksti1 + ".xml");
                     byte[] buffer = new byte[1024];
                     int length;
                     while ((length = is.read(buffer)) > 0) {
