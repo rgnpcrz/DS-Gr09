7e90e75ac080fa1f6b7690f31b58722016d53a61
samiri32 <58012577+samiri32@users.noreply.github.com>
2020-05-06 22:05:14 +0200

    Add files via upload
    Nje pjese e writeMessage

--- ds/src/ds/WriteMessage.java
+++ ds/src/ds/WriteMessage.java
@@ -1,34 +1,19 @@
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
 public class WriteMessage {
 
 
-    public static void WriteMessage(String teksti1, String teksti2) throws UnsupportedEncodingException, NoSuchAlgorithmException {
-        int pldh = 111;
-        byte[] arr = new byte[pldh];
-        SecureRandom prng = SecureRandom.getInstance("SHA1PRNG");
-        prng.nextBytes(arr);
-
-        byte[] arr1 = new byte[pldh];
-        SecureRandom prng1 = SecureRandom.getInstance("SHA1PRNG");
-        prng1.nextBytes(arr1);
-
-
+    public static void WriteMessage(String teksti1, String teksti2) throws Exception {
         String base64encodedString = Base64.getEncoder().encodeToString(
                 teksti1.getBytes("utf-8"));
-
+        SecureRandom prng = SecureRandom.getInstance("SHA1PRNG");
         String base64encodedString1 = Base64.getEncoder().encodeToString(
-                teksti2.getBytes("utf-8"));
+                teksti2.getBytes("prng"));
 
-        String base64encodedString2 = Base64.getEncoder().encodeToString(
-                prng1.getBytes("utf-8"));
-
-        String ciphertext = base64encodedString + "." + arr + "." + arr1 + "." + base64encodedString1;
+        String ciphertext = base64encodedString + "." + base64encodedString1 ;
         System.out.println(ciphertext);
 
     }
+
+
 }
-
-
